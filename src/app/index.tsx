932ba947204475d6392a65e0048b107237ad2867
alnnu <luannf1020@gmail.com>
2024-11-27 12:05:36 -0300

    add: menu

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,30 @@
-import {Text, View} from "react-native";
+import React, {useState} from "react";
+import {Text, TouchableOpacity, View} from "react-native";
 
 export default function RootLayout() {
 
-    const tabs: {name:String, ref:String}[] = [{name: "Todos Itens", ref:"allItnes"}, {name:"lista", ref:"list"}, {name:"Ultimas Compras", ref:"Last"}]
+    const tabsRef: {name:string, value:string}[] = [{name: "Todos Itens", value:"allItnes"}, {name:"lista", value:"list"}, {name:"Ultimas Compras", value:"Last"}]
+
+    const [selectedTab, setSelectedTab] = useState<string>("allItnes")
+
+    const handleChangeTab = (tabName: string) => {
+        setSelectedTab(tabName)
+    }
+
     return (
-        <View>
+        <View style={{paddingHorizontal: 16, paddingVertical: 12}}>
             <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
                 {
-                    tabs.map((tab, i) => (
-                        <Text key={i}>{tab.name}</Text>
+                    tabsRef.map((tabRef) => (
+                        <TouchableOpacity key={tabRef.value} onPress={() => handleChangeTab(tabRef.value)}>
+                            <Text style={{ fontSize: selectedTab === tabRef.value ? 18 : 12, color: "#000", borderBottomWidth: selectedTab === tabRef.value ? 1 : 0, borderBottomColor: "#058EFF" }}>{tabRef.name}</Text>
+                        </TouchableOpacity>
                     ))
                 }
+            </View>
+
+            <View>
+                <Text style={{ textAlign: "center", fontSize: 24, fontWeight: "bold", marginTop: 20 }}>Em breve</Text>
             </View>
         </View>
     )
